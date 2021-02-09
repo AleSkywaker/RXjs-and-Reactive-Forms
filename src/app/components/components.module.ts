ae403b7d965c5aa394367f5855d1b036124d9fdc
Alex Colombo <uricaine@hotmail.com>
2021-02-09 21:14:31 +0100

    new features

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
-  declarations: [TableComponent],
+  declarations: [TableComponent, DetailsComponent],
   imports: [CommonModule, MatTableModule, MatSortModule],
   exports: [TableComponent],
 })
